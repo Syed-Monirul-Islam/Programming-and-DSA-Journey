59e3f5fe9ae8da036a9f939e68664bff7fc8dd04
Moni <syedmonirulislamcu@gmail.com>
2026-04-23 11:33:03 +0600

    Solve Pangram (520A) using lowercase conversion and frequency array in C

---
+++ Introduction To Programing/Module11.5/A_Pangram.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+ 
+    int n;
+    scanf("%d",&n);
+    char s[n+1];
+    scanf("%s",s);
+
+    for (int i=0; i<n; i++)
+    {
+      if(s[i]>='A' && s[i]<='Z')
+      {
+        s[i]+=32;
+      }
+    }
+    int fre[26]={0};
+    for(int i=0;i<n; i++)
+    {
+        int val = s[i];
+        fre[s[i]-'a']++;
+    }
+    int result=0;
+    for(int i=0;i<26;i++)
+    {
+
+        if(fre[i] !=0)
+        {
+            result++;
+        }
+    }
+    if(result ==26)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+
+  return 0;
+}
