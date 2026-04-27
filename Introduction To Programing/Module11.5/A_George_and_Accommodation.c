1f33d72726f7b525eed66e58e5c25e3a1408b6d1
Moni <syedmonirulislamcu@gmail.com>
2026-04-27 23:43:01 +0600

    Solve Codeforces 467A - George and Accommodation

---
+++ Introduction To Programing/Module11.5/A_George_and_Accommodation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, p, q;
+    int count = 0;
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        
+        scanf("%d %d", &p, &q);
+        if (q - p >= 2) {
+            count++;
+        }
+    }
+    
+    printf("%d\n", count);
+    
+    return 0;
+}
