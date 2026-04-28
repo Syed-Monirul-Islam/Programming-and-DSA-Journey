6f6ebd4010c0bdedeef8cc284eadbe9ce3bd7e18
Moni <syedmonirulislamcu@gmail.com>
2026-04-28 18:41:49 +0600

    Solve Codeforces 344A - Magnets

---
+++ Introduction To Programing/Module11.5/A_Magnets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+  int n;
+  scanf("%d",&n);
+
+  int groups=1;
+  int prev,current;
+  scanf("%d",&prev);
+ 
+  for (int i=1; i<n; i++)
+  {
+      scanf("%d",&current);
+  {
+    if(current !=prev)
+    {
+        groups++;
+        prev = current;
+    }
+  }
+  }
+
+ printf("%d\n",groups);
+
+  return 0;
+}
