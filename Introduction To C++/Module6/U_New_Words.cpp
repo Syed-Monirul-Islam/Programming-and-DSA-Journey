b023640b2fd641fb49b2cbd49ac046d6918f42f7
Moni <syedmonirulislamcu@gmail.com>
2026-06-08 20:51:23 +0600

    Solve Codeforces New Words string character counting problem in C++

---
+++ Introduction To C++/Module6/U_New_Words.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string s;
+   cin >> s;
+   int e=0,g=0, y=0,p=0,t=0;
+
+   for(char c : s)
+   {
+    if( c=='e' || c == 'E') e++;
+    else if( c=='g' || c == 'G') g++;
+    else if( c=='y' || c == 'Y') y++;
+    else if( c=='p' || c == 'P') p++;
+    else if( c=='t' || c == 'T') t++;
+   }
+
+   int ans = min({e,g,y,p,t});
+
+   cout << ans << endl;
+    return 0;
+}
