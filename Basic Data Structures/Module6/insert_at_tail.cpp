8db3a5b44c88cb58c33f5c8e922fc246ccb79aeb
Moni <syedmonirulislamcu@gmail.com>
2026-06-30 21:03:02 +0600

    Add insert at tail and print list functions

---
+++ Basic Data Structures/Module6/insert_at_tail.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+     int val;
+     Node* next;
+
+   Node (int val)
+   {
+     this->val =val;
+     this->next=NULL;
+   }
+
+    
+};
+
+void insert_tail(Node* &head, int val)
+{
+    Node* newnode =new Node(val);
+    if(head == NULL)
+    {
+        head= newnode;
+        return;
+    }
+
+    Node* tmp= head;
+    while(tmp->next !=NULL)
+    {
+        tmp =tmp->next;
+
+    }
+    tmp->next= newnode;
+}
+
+void print_link_list(Node* head)
+{
+    Node* tmp =head;
+    while(tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a= new Node(20);
+
+    head->next=a;
+    insert_tail(head,30);
+    print_link_list(head);
+   
+    return 0;
+}
